316e38d1d8c9187a05adb080bed5cf8f76189be3
yilkid <yillkid@gmail.com>
2020-08-11 08:54:10 +0000

    feat: credential editer API

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-from apps.tangle import write_data_to_tangle 
+from apps.tangle import write_data_to_tangle
 
 @app.route("/")
 def issuer_form():
     return render_template('issuer.html')
 
-@app.route("/write_data")
+@app.route("/write_data", methods=['GET', 'POST'])
 def write_data():
-    result = write_data_to_tangle({"date":"2020-08-11"})
-    return str(result)
+    if request.method == 'POST':
+        result = write_data_to_tangle(request.form['message'])
+        return str(result)
+
+@app.route("/credential_editor")
+def credential_editor():
+    return render_template('credential_editor.html')
 
 if __name__ == "__main__":
     app.run(debug = True, threaded = True, host = "0.0.0.0", port = 5000)
